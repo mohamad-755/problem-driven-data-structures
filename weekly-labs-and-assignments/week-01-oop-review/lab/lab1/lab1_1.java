984bf1073bc4babbcee8790e4bba0be0a808b52f
mohamad-755 <mohammadziadah212@gmail.com>
2026-02-12 21:24:53 +0200

    Improve OOP summary in README

---
+++ weekly-labs-and-assignments/week-01-oop-review/lab/lab1/lab1_1.java
@@ -0,0 +1,150 @@
+package lab1;
+
+abstract class SmartDevice {
+    private boolean isOn;
+
+    public SmartDevice(boolean isOn) {
+        this.isOn = isOn;
+    }
+
+    public SmartDevice() {
+        this(true);
+    }
+
+    public SmartDevice(SmartDevice other) {
+        this.isOn = other.isOn;
+    }
+
+    public boolean getIsOn() {
+        return isOn;
+    }
+
+    public void setIsOn(boolean isOn) {
+        this.isOn = isOn;
+    }
+
+    public void turnOn() {
+        isOn = true;
+    }
+
+    public void turnOff() {
+        isOn = false;
+    }
+
+    public abstract void reset();
+
+    public abstract void statusReport();
+}
+
+class SmartLight extends SmartDevice {
+    private int brightness;
+
+    public SmartLight(boolean isOn, int brightness) {
+        super(isOn);
+        if (brightness >= 0 && brightness <= 100) {
+            this.brightness = brightness;
+        } else {
+            System.out.println("Incorrect value!");
+        }
+    }
+
+    public SmartLight() {
+        super(true);
+        this.brightness = 50;
+    }
+
+    public int getBrightness() {
+        return brightness;
+    }
+
+    public void setBrightness(int brightness) {
+        this.brightness = brightness;
+    }
+
+    public void reset() {
+        setBrightness(50);
+        setIsOn(true);
+    }
+
+    public void statusReport() {
+        boolean x = getIsOn();
+        if (x) {
+            System.out.println("Smart light is On with brightness set to " + brightness);
+        } else {
+            System.out.println("Smart light is Off with brightness set to " + brightness);
+        }
+    }
+
+    public void dimLight() {
+        if (brightness == 0) {
+            System.out.println("Brightness is already low!");
+        } else {
+            brightness -= 10;
+        }
+    }
+
+    public void brightenLight() {
+        if (brightness == 100) {
+            System.out.println("Brightness is already high!");
+        } else {
+            brightness += 10;
+        }
+    }
+}
+
+class SmartThermostat extends SmartDevice {
+    private int temp;
+
+    public SmartThermostat(boolean isOn, int temp) {
+        super(isOn);
+        this.temp = temp;
+    }
+
+    public SmartThermostat() {
+        super(true);
+        this.temp = 22;
+    }
+
+    public int getThermostat() {
+        return temp;
+    }
+
+    public void setThermostat(int temp) {
+        this.temp = temp;
+    }
+
+    public void reset() {
+        setIsOn(true);
+        setThermostat(22);
+    }
+
+    public void statusReport() {
+        boolean x = getIsOn();
+        if (x) {
+            System.out.println("Smart Thermostat is On with temperature set to " + temp);
+        } else {
+            System.out.println("Smart Thermostat is Off with temperature set to " + temp);
+        }
+    }
+}
+
+public class lab1_1 {
+    public static void main(String[] args) {
+        SmartDevice[] smartHome = new SmartDevice[5];
+        smartHome[0] = new SmartLight();
+        smartHome[1] = new SmartLight();
+        smartHome[2] = new SmartThermostat();
+        smartHome[3] = new SmartLight();
+        smartHome[4] = new SmartThermostat();
+        for (SmartDevice a : smartHome) {
+            if (a instanceof SmartLight) {
+                ((SmartLight) a).dimLight();
+                ((SmartLight) a).turnOff();
+            } else if (a instanceof SmartThermostat) {
+                // ((SmartThermostat)a).turnOff();
+                ((SmartThermostat) a).setThermostat(18);
+            }
+            a.statusReport();
+        }
+    }
+}
---
+++ weekly-labs-and-assignments/week-01-oop-review/lab/lab1/lab1_2.java
@@ -0,0 +1,81 @@
+package lab1;
+
+import java.util.*;
+
+abstract class Animal {
+    private String name;
+    private int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void makeSound();
+
+    public void displayInfo() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
+
+class Dog extends Animal {
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    public void makeSound() {
+        System.out.println("Dog: Woof!");
+    }
+}
+
+class Cat extends Animal {
+    public Cat(String name, int age) {
+        super(name, age);
+    }
+
+    public void makeSound() {
+        System.out.println("Cat: Meow!");
+    }
+}
+
+class Zoo {
+    private ArrayList<Animal> a;
+
+    public Zoo() {
+        a = new ArrayList<>();
+    }
+
+    public void addAnimal(Animal x) {
+        a.add(x);
+    }
+
+    public void soundOff() {
+        for (Animal x : a) {
+            x.makeSound();
+        }
+    }
+
+    public void displayAll() {
+        for (Animal x : a) {
+            x.displayInfo();
+        }
+    }
+}
+
+public class lab1_2 {
+    public static void main(String[] args) {
+        Zoo q = new Zoo();
+        q.addAnimal(new Dog("Buddy", 3));
+        q.addAnimal(new Cat("Luna", 2));
+        q.soundOff();
+        q.displayAll();
+    }
+}
